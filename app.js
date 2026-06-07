ac930da799e6e91fa342d532af508184d401f650
Sangyae <bibekplus100@gmail.com>
2026-06-07 16:34:36 +0100

    static method and usage like total doors we can access

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class car {
+class Car {
     constructor(doors, engine , color){
         this.doors = doors;
         this.engine = engine;
@@ -8,8 +8,30 @@ class car {
     carStats(){
         return `This car has ${this.doors} doors, a ${this.engine} engine and is ${this.color} color! `;
 }
+
+    static totalDoors(Car1, Car2){
+        const doors1 = Car1.doors;
+        const doors2 = Car2.doors;
+
+        return doors1 + doors2;
+    }
 }
 
-const car1 = new car(4, "V8", "red");
-console.log(car1);
-console.log(car1.carStats());   
+const cx1 = new Car(4, "V8", "red");
+const cx2 = new Car(2, "v2", "blue");
+
+console.log(cx1);
+console.log(cx1.carStats()); 
+
+console.log(cx2);
+console.log(cx2.carStats());
+
+console.log(Car.totalDoors(cx1, cx2));
+
+
+
+function sayHi(){
+    console.log("Hi");
+}
+
+sayHi();
